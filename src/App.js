5480a2dfbe7045791b653f754b0e5389b9805b6f
lzvckaya <lzvckaya@gmail.com>
2023-08-29 23:57:17 +0900

    add state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  
-  let post = '강남 우동 맛집';
-  
-  
+  let postName = ["강남우동맛집", "남자코트 추천", "파이썬 독함"];
+  let [title1, m1] = useState(postName[0]);
+  let [title2, m2] = useState(postName[1]);
+  let [title3, m3] = useState(postName[2]);
 
   return (
     <div className="App">
-      <div className="black-nav"> // style 넣을때는 중괄호 style {}
+      <div className="black-nav">
+        {" "}
+        {/*style 넣을때는 중괄호 style {}*/}
         <h4>데일리 라이프</h4>
       </div>
-      <h4>{post}</h4>  // document.querySelector('h4').innerHTML = post; 데이터 바인딩
+      <div className="list">
+        <h4>{title1}</h4>
+        <p>2월 17발행</p>
+      </div>
+      <div className="list">
+        <h4>{title2}</h4>
+        <p>2월 17발행</p>
+      </div>
+      <div className="list">
+        <h4>{title3}</h4>
+        <p>2월 17발행</p>
+      </div>
+      {/* document.querySelector('h4').innerHTML = post; 데이터 바인딩 */}
     </div>
   );
 }
