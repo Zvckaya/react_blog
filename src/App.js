48d2e55db7bfd82e60c6088ec17dab467f98538b
lzvckaya <lzvckaya@gmail.com>
2023-08-29 17:18:53 +0900

    for upload

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  let post = '강남 우동 맛집';
+  
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav"> // style 넣을때는 중괄호 style {}
+        <h4>데일리 라이프</h4>
+      </div>
+      <h4>{post}</h4>  // document.querySelector('h4').innerHTML = post; 데이터 바인딩
     </div>
   );
 }
