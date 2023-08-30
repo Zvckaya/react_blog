4d26ee3682fcae0c75a71d46a151f4ccfeb388df
lzvckaya <lzvckaya@gmail.com>
2023-08-30 23:01:18 +0900

    add modal with map

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   let postName = ["여자코트 추천", "강남 우동맛집", "파이썬 독학"];
   let [title, mt] = useState(postName);
   let [goods, mg] = useState(0);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
@@ -16,37 +17,39 @@ function App() {
         <h4>데일리 라이프</h4>
       </div>
 
-      <button
-        onClick={() => {
-          let copy = [...title];
-          copy = copy.sort();
-          mt(copy);
-        }}>
-        가나다순 정렬
-      </button>
-      <div className="list">
-        <h4>
-          {title[0]}{" "}
-          <span
-            onClick={() => {
-              mg(goods + 1);
-            }}>
-            👍
-          </span>{" "}
-          {goods}
-          {/* onClick 은 이벤트 핸들러이다. */}
-        </h4>
-        <p>2월 17발행</p>
-      </div>
-      <div className="list">
-        <h4>{title[1]}</h4>
-        <p>2월 17발행</p>
-      </div>
-      <div className="list">
-        <h4>{title[2]}</h4>
-        <p>2월 17발행</p>
-      </div>
+      {title.map(function (title, i) {
+        console.log(i);
+        return (
+          <div className="list">
+            <h4>
+              {title}{" "}
+              <span
+                onClick={() => {
+                  mg(goods + 1);
+                }}>
+                👍
+              </span>{" "}
+              {goods}
+              {/* onClick 은 이벤트 핸들러이다. */}
+            </h4>
+            <p>2월 17발행</p>
+          </div>
+        );
+      })}
+
+      {modal === true ? <Modal /> : null}
+
       {/* document.querySelector('h4').innerHTML = post; 데이터 바인딩 */}
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
